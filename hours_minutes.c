8fc13039ea918f156e548f3e83e52694b0acd69a
HELLO-world8999 <92536558+HELLO-world8999@users.noreply.github.com>
2021-10-14 21:33:26 +0530

    Add files via upload

---
+++ hours_minutes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{int main_value , minutes , hours ;
+printf("Enter your value: ");
+scanf("%d",&main_value);
+if (main_value>=100 && main_value<=999){
+
+    minutes=main_value%100;
+    hours=main_value//100;
+    if (minutes==00){
+        printf("%d hours",hours);}
+    else{
+        printf("%d hours   %d minutes",hours,minutes);}
+        
+}
+else if (main_value>=10 && main_value<=99){
+    printf("%d minutes,main_value");
+}
+
+return 0;
+}
